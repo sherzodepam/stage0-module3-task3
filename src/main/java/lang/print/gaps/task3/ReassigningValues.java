33774ea6ed4c6da3a00cb886e4c01f32bf32a0f5
Sherzod <sherzodd7707@gmail.com>
2022-08-21 13:02:08 +0500

    I learnt how to work with variables

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,25 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first=1;
+        int second=10;
+        int third=100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        int linkToFirst=first;
+        int linkToSecond=second;
+        int linkToThird=third;
+        int linktoFirst=15;
+        int linktoSecond=6;
+        int linktoThird=4;
+        System.out.println(linktoFirst);
+        System.out.println(linktoSecond);
+        System.out.println(linktoThird);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+
     }
 }
